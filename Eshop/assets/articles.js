91ce2dc43e192e707fa452d8acfdc24dd1d44dc5
Axel Somian <axelsomian2@gmail.com>
2023-11-03 10:27:05 +0000

    Creation du dossier eshop affichage des article sur index

---
+++ Eshop/assets/articles.js
@@ -0,0 +1,57 @@
+const articles=[
+    {
+        id:1,
+        cover:'./assets/azusRog.png',
+        name:'Azus rog',
+        description:'PC gamming',
+        price:500
+    },
+    {
+        id:2,
+        cover:'./assets/sonyA7.png',
+        name:'Sony 7A',
+        description:'Full-frame camera',
+        price:2000
+    },
+    {
+        id:3,
+        cover:'./assets/iphone15.png',
+        name:'iphone 15 pro',
+        description:'1To ',
+        price:930
+    },
+    {
+        id:4,
+        cover:'./assets/teslaModel3.png',
+        name:'Tesla model 3',
+        description:'Couleur rouge ',
+        price:150000
+    },
+    {
+        id:1,
+        cover:'./assets/azusRog.png',
+        name:'Azus rog',
+        description:'PC gamming',
+        price:500
+    },
+    {
+        id:2,
+        cover:'./assets/sonyA7.png',
+        name:'Sony 7A',
+        description:'Full-frame camera',
+        price:2000
+    },
+    {
+        id:3,
+        cover:'./assets/iphone15.png',
+        name:'iphone 15 pro',
+        description:'1To ',
+        price:930
+    },
+    {
+        id:4,
+        cover:'./assets/teslaModel3.png',
+        name:'Tesla model 3',
+        description:'Couleur rouge ',
+    }
+]
